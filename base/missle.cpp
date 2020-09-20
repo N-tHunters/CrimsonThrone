9363e0a25bb4a6eaa41ce4ea412aaeeb2e0b2a39
Lukianov Artur <artur.lukyanov@gmail.com>
2020-09-20 19:54:30 +0300

    Gitignore

--- base/missle.cpp
+++
@@ -1,6 +0,0 @@
-#include "missle.h"
-#include "entries.h"
-
-std::stringstream Missle::Save(Saver* saver) {
-  return StackableItem::Save(saver, ENTRY_MISSLE);
-}
--- base/player.h
+++
@@ -1,8 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "actor.h"
-
-class Player : public Actor {};
-
-#endif
