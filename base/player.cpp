7fcdac5b3573b2474a3c460a7009f6d82c963847
Lukianov Artur <artur.lukyanov@gmail.com>
2020-09-30 11:33:21 +0300

    Player uses camera now

--- base/player.cpp
+++ base/player.cpp
@@ -2,3 +2,13 @@
 
 Player::Player() : Actor() {}
 
+Player::Player(std::string name, int max_health, PhysicalObj obj, Camera * camera) :
+  Actor(name, max_health, obj)
+{
+  this->camera = camera;
+}
+
+
+void Player::Update() {
+  this->camera->setPosition(this->GetPhysicalObj()->getPosition());
+}
