e7695f1b760e7c8199f7f6abe6d061d3a9c1ab7b
Lukianov Artur <artur.lukyanov@gmail.com>
2020-09-30 18:15:49 +0300

    Player fixed

--- base/player.cpp
+++ base/player.cpp
@@ -10,5 +10,15 @@ Player::Player(std::string name, int max_health, PhysicalObj obj, Camera * camer
 
 
 void Player::Update() {
+  this->GetPhysicalObj()->update();
   this->camera->setPosition(this->GetPhysicalObj()->getPosition());
+}
+
+
+Camera * Player::GetCamera() {
+  return this->camera;
+}
+
+void Player::SetCamera(Camera * camera) {
+  this->camera = camera;
 }
