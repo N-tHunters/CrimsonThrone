c3d3b914b590ebdbc68708ea061420144a1d17b6
Altrul <AlexanderOnko@gmail.com>
2020-11-13 10:45:51 +0300

    AAAA

--- UI/column.cpp
+++ UI/column.cpp
@@ -80,7 +80,7 @@ Column::Column(glm::vec4 rect, std::vector<AbstractListElement*>* list, std::str
 	glBindVertexArray(0);
 
 	for(int i = 0; i < list.size(); i ++) {
-		//this->column.push_back(Text(list[i].))
+		this->column.push_back(Text(list[i].getValues[number], rect, Characters, 0.001f, glm::vec3(255, 255, 0)));
 	}
 }
 
@@ -96,4 +96,8 @@ void Column::draw(ShaderHolder* shaderHolder) {
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
+
+	for(int i = 0; i < this->column.size(); i ++) {
+		this->column[i].draw(shaderHolder);
+	}
 }
