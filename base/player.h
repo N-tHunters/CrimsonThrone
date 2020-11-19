3c0c90f74b2b0b736d60f0ce2652bcb5337ea2a1
Lukianov Artur <artur.lukyanov@gmail.com>
2020-11-19 07:53:34 +0300

    Docs for Actor are finsished, make docs for player

--- base/player.h
+++ base/player.h
@@ -1,9 +1,17 @@
+/**
+ * \file
+ * \brief Declaration of Player
+ */
 #ifndef PLAYER_H
 #define PLAYER_H
 
 #include "actor.h"
 #include "../render/camera.h"
 
+/**
+ * \brief Human controlled game character
+ * Player is a human controlled game character. It uses camera to render his point of view and acts like any other character in other ways.
+ */
 class Player : public Actor {
 private:
   Camera * camera;
