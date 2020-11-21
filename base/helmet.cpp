f3b11970ee567bd3805896cfd15ef44c94111f52
Lukianov Artur <artur.lukyanov@gmail.com>
2020-11-21 11:27:39 +0300

    Helmet documented

--- base/helmet.cpp
+++ base/helmet.cpp
@@ -1,18 +1,36 @@
+/**
+ * \file
+ * \brief This file contains implementation of Helmet
+ */
 #include "helmet.h"
 #include "actor.h"
 #include "entries.h"
 
+/**
+ * Basic constructor implemented from Armor. Just the same.
+ */
 Helmet::Helmet(std::string name, PhysicalObj * obj, int defence) :
   Armor(name, obj, defence) {}
 
+/**
+ * Constructor without PhysicalObj. Just the same as in Armor.
+ */
 Helmet::Helmet(std::string name, int defence) :
   Helmet(name, new PhysicalObj(), defence) {}
 
+/**
+ * Wear helmet on target.
+ * \param target Who wears helmet
+ */
 void Helmet::Wear(Actor * target) {
   target->WearHelmet(this);
 }
 
-
+/**
+ * Convert object to binary stream.
+ * \param saver Serializer
+ * \return Binary stream representing this object
+ */
 std::stringstream * Helmet::Save(Saver * saver) {
   return Armor::Save(saver, ENTRY_HELMET);
 }
