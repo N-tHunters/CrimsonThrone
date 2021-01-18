c847eb2cab85bedf07c11e6b7a4a5ebe6094eabf
Lukianov Artur <artur.lukyanov@gmail.com>
2021-01-18 19:32:57 +0300

    include problems resolved

--- render/model.hpp
+++ render/model.hpp
@@ -1,3 +1,6 @@
+#ifndef MODEL_H
+#define MODEL_H
+
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -13,3 +16,5 @@ class Model
         void loadModel(string path);
         void processNode(aiNode *node, const aiScene *scene);
 };
+
+#endif
