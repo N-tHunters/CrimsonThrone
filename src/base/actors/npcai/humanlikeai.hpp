e25d09b83a698bd85b1d1a539d5baf615648673d
Artur <ucat@chaosetemple>
2021-04-19 17:10:15 +0300

    Humanlike NPCs started (hunger)

--- src/base/actors/npcai/humanlikeai.hpp
+++ src/base/actors/npcai/humanlikeai.hpp
@@ -2,9 +2,31 @@
 
 #include <base/actors/npcai/wanderai.hpp>
 
+#define SATISFY_HUNGER 1
+
 class HumanlikeAI : WanderAI {
 protected:
+  double hunger;
+  double fatigue;
+  double social_dissatisfaction;
+  double individuality;
+
+  double hunger_threshold;
+  double gluttony_priority_modifier;
+
+  double greed_modifier;
+
+  unsigned int flags;
+
+  double work_intensity;
+  
 public:
   HumanlikeAI();
+
+  unsigned int GetFlags();
+  void SetFlags();
+  
+  void SetFlag(int flag);
+  void UnsetFlag(int flag);
   //  virtual void Process();
 };
