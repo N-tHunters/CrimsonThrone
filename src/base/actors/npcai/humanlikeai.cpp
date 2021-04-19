6208016c8f42d23931b3fcb678d7c5d719aea9a7
Artur <ucat@chaosetemple>
2021-04-19 17:10:15 +0300

    Humanlike NPCs started (hunger)

--- src/base/actors/npcai/humanlikeai.cpp
+++ src/base/actors/npcai/humanlikeai.cpp
@@ -1,3 +1,14 @@
 #include "humanlikeai.hpp"
 
-HumanlikeAI::HumanlikeAI() {}
+HumanlikeAI::HumanlikeAI() {
+  this->hunger = 1000000;
+  this->hunger_threshold = 100000;
+  this->gluttony_priority_modifier = 1.1;
+  this->greed_modifier = 1.2;
+  
+  this->flags = 0;
+}
+
+unsigned int HumanlikeAI::GetFlags() {
+  return flags;
+}
