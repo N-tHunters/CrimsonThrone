cef9407d9def49d714b22ef238f4a836b409f3b6
Magnum Malum <no@mail>
2021-10-12 17:32:35 +0300

    flat world for testing

--- src/base/location/worldmap.cpp
+++ src/base/location/worldmap.cpp
@@ -5,6 +5,7 @@
 #include <base/location/location.hpp>
 #include <base/triggers/longjumptrigger.hpp>
 #include <base/items/weapon.hpp>
+#include <physics/boundary.hpp>
 #include <landscape/flatgenerator.hpp>
 #include <landscape/overworldgenerator.hpp>
 #include <landscape/dungeona1generator3d.hpp>
@@ -16,27 +17,26 @@
 std::unordered_map<int, Location *> location_map;
 
 void init_demo_locations() {
-  Location * open_world = new Location(100, 100, 100, 100, new OverWorldGenerator());
+  Location * open_world = new Location(4, 4, 100, 100, new FlatGenerator());
   location_map[0] = open_world;
 
   /*std::vector<std::tuple<Chunk *, glm::vec3>> exits;
-    exits.push_back(std::make_tuple(open_world->GetChunk(0, 0), glm::vec3(2.0f, 2.0f, 2.0f)));
+  exits.push_back(std::make_tuple(open_world->GetChunk(0, 0), glm::vec3(2.0f, 2.0f, 2.0f)));
 
-    Model* portal_model = new Model("resources/models/portal.obj");
+  Model* portal_model = get_model("portal");
 
-    for(int i = 1; i < 4; i++) {
-    for(int j = 1; j < 4; j++) {
-    location_map[(i - 1) * 4 + j] = new Location(5, 5, 10, 10, new DungeonA1Generator3D(5, &exits));
-    PhysicalObj * portal = new PhysicalObj(new Mesh("resources/textures/test.jpg", portal_model),
-    false, true, false, false,
-    glm::vec3(5.f + 10.f * i, 3.f, 5.f + 10.f * j),
-    glm::vec3(0.0f, 0.0f, 0.0f),
-    "portal",
-    new BoundaryBox(1.0f, 1.0f, 1.0f));
-    location_map[0]->GetChunk(i, j)->AddObj(portal);
-    location_map[0]->GetChunk(i, j)->AddTrigger(new LongJumpTrigger(portal, glm::vec3(2.0f, 2.0f, 2.0f), location_map[(i - 1) * 4 + j]->GetChunk(0, 0)));
-    }
-    }*/
+  location_map[1] = new Location(5, 5, 10, 10, new DungeonA1Generator3D(5, &exits));
+  PhysicalObj * portal = new PhysicalObj(new Mesh(portal_model, get_texture("fire")),
+					 false, true, false, false,
+					 glm::vec3(5.f + 10.f, 3.f, 5.f),
+					 glm::vec3(0.0f, 0.0f, 0.0f),
+					 "portal",
+					 get_model("portal")->getBoundaryBox(1.0f));
+  location_map[0]->GetChunk(0, 0)->AddObj(portal);
+  location_map[0]->GetChunk(0, 0)->AddTrigger(new LongJumpTrigger(portal, glm::vec3(2.0f, 2.0f, 2.0f), location_map[1]->GetChunk(0, 0)));
+
+
+  
   Actor * last_actor = nullptr;
 
   for(int i = 0; i < 5; i++){
@@ -55,7 +55,7 @@ void init_demo_locations() {
     /*ParticleEmitter* test_particle_emitter = new ParticleEmitter(*(test_actor->GetPhysicalObj()), get_texture("void"));
 
       open_world->GetChunk(0, 0)->AddParticleEmitter(test_particle_emitter);*/
-  }
+  /*  }
 
   
   for(int i = 0; i < 3; i++){
@@ -65,7 +65,7 @@ void init_demo_locations() {
 						       get_model("sword")->getBoundaryBox(1.0f)), 5, 10);
     open_world->GetChunk(0, 0)->AddItem(totacres_wep);
     
-  }
+  }*/
 
   SetCurrentLocation(open_world);
   open_world->LoadABS();
