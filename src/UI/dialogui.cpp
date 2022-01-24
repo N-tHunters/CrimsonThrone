f9b12d4a91ac9f95700228b4253dc39de9203561
Altrul <AlexanderOnko@gmail.com>
2022-01-24 17:17:33 +0300

    actor to npc

--- src/UI/dialogui.cpp
+++ src/UI/dialogui.cpp
@@ -1,13 +1,13 @@
 #include <UI/dialogui.hpp>
 
-DialogUI::DialogUI(Actor* actor, std::map<GLchar, Character> characters) {
-	m_actor = actor;
-	m_actor_line = actor->getLine();
-	m_player_lines = actor->getPlayerLines();
+DialogUI::DialogUI(NPC* npc, std::map<GLchar, Character> characters) {
+	m_npc = npc;
+	m_npc_line = npc->getLine();
+	m_player_lines = npc->getPlayerLines();
 
-	m_actor_line_text = new Text(m_actor_line, characters, 1.0f, glm::vec3(1), glm::vec2(0.0f, 0.5f));
+	m_npc_line_text = new Text(m_npc_line, characters, 1.0f, glm::vec3(1), glm::vec2(0.0f, 0.5f));
 }
 
 void DialogUI::draw(ShaderHolder* shaderHolder) {
-	m_actor_line_text->draw(shaderHolder);
+	m_npc_line_text->draw(shaderHolder);
 }
--- src/UI/dialogui.hpp
+++ src/UI/dialogui.hpp
@@ -4,13 +4,13 @@
 #include <base/actors/actor.hpp>
 
 class DialogUI {
-	Actor* m_actor;
-	std::string m_actor_line;
+	NPC* m_npc;
+	std::string m_npc_line;
 	std::vector<std::pair<int, std::string>> m_player_lines;
 
-	Text* m_actor_line_text;
+	Text* m_npc_line_text;
 	std::vector<Text*> m_player_lines_text;
 public:
-	DialogUI(Actor* actor);
+	DialogUI(NPC* npc);
 	draw(ShaderHolder*);
 }
